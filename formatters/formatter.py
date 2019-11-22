6a43621ec4d46ea4dd86427be6342571861af3f1
tugbaalparslan <tugba.alparslan@outlook.com>
2019-11-22 13:07:54 -0800

    code improvement for ex-28 (readable dictionary - formatted)

--- formatters/formatter.py
+++ formatters/formatter.py
@@ -1,2 +1,8 @@
 def json_user_object(user):
-    return {"email": user.email, "name": user.name, "last_name": user.last_name, "phone": user.phone, "gender": user.gender}
+    return {
+        "email": user.email,
+        "name": user.name,
+        "last_name": user.last_name,
+        "phone": user.phone,
+        "gender": user.gender
+    }
